4a762fbd00788800f52b7ab1a34480803e4b360f
isenberg.john.1925@mail.ru <isenberg.john.1925@mail.ru>
2022-02-17 16:24:00 +0300

    2022-02-17 search from the begining

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace RemoteConnections
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(116, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Подключиться";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Имя компьютера:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Статус:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(116, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(192, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 70);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(162, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Подключиться с запросом";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 99);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Теневое подключение";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.DirectoryServices;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TerminalTools;
+
+namespace RemoteConnections
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+                       
+        }
+
+        //Кнопка "Подключиться"
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Connect();
+        }
+
+        //Событие при нажатии ENTER
+        private void TextBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                Connect();
+        }
+
+        //Подключение 
+        private void Connect()
+        {
+            this.Cursor = Cursors.WaitCursor;
+
+            string server = comboBox1.Text;
+            string message = "";
+            var x = TermServicesManager.ListSessions(server);
+            if (x.Count > 0)
+                foreach (var item in x)
+                {
+                    if (item.ConnectionState.ToString() == "Active")
+                    {
+                        if (checkBox1.Checked)
+                        {
+                            message = TermServicesManager.GetSessionInfo(server, item.SessionId).UserName;
+                            Process.Start("mstsc.exe", $@"/shadow:{item.SessionId} /control /v:{server}");
+                        }
+                        else
+                        {
+                            message = TermServicesManager.GetSessionInfo(server, item.SessionId).UserName;
+                            Process.Start("mstsc.exe", $@"/shadow:{item.SessionId} /control /noConsentPrompt /v:{server}");
+                        }
+                    }
+                }
+            else
+                message = "Недоступен";
+            if (message == "")
+                message = "Не подключено";
+            textBox1.Text = message;
+
+            this.Cursor = Cursors.Default;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = comboBox1;
+            GetComputersFromAD();
+            //comboBox1.DroppedDown = true;
+
+
+        }
+
+        private void GetComputersFromAD()
+        {
+            var root = new DirectoryEntry("LDAP://" + "oilpro");
+            var departmentSet = root.Children.Find("OU=Departments");
+            List<string> strList = new List<string>();
+            foreach (DirectoryEntry depart in departmentSet.Children)
+            {
+                if (depart.Children != null)
+                {
+                    bool isSuccess = true;
+                    DirectoryEntry compSet = new DirectoryEntry();
+                    try
+                    {
+                        compSet = depart.Children.Find("OU=Computers");
+                    }
+                    catch
+                    {
+                                
+                        try
+                        {
+                            compSet = depart.Children.Find("OU=GAPR");
+                            foreach (DirectoryEntry comp in compSet.Children)
+                            {
+                                                             
+                                    foreach (DirectoryEntry c in comp.Children)
+                                    {
+                                        var offset = c.Name.IndexOf("=") + 1;
+                                        strList.Add(c.Name.Substring(offset));
+                                    }
+                                
+                            }
+                        }
+                        catch
+                        {
+                            isSuccess = false;
+                        }
+                    }
+                    if (isSuccess)
+                    {
+                        if (compSet != null)
+                        {
+
+                            foreach (DirectoryEntry comp in compSet.Children)
+                            {
+                                var offset = comp.Name.IndexOf("=") + 1;
+                                strList.Add(comp.Name.Substring(offset));
+                            }
+                        }
+                            
+                           
+                        
+                    }
+                }
+            }
+            var strListOr = strList.OrderBy(x => x);
+            foreach (var str in strListOr)
+            {
+                comboBox1.Items.Add(str);
+            }
+            
+        }
+
+        int count = 1;
+        string text = string.Empty;
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            object[] originalList = (object[])comboBox1.Tag;
+            if (originalList == null)
+            {
+                // backup original list
+                originalList = new object[comboBox1.Items.Count];
+                comboBox1.Items.CopyTo(originalList, 0);
+                comboBox1.Tag = originalList;
+            }
+
+            // prepare list of matching items
+            string s = comboBox1.Text;
+            IEnumerable<object> newList = originalList;
+            if (s.Length > 0)
+            {
+                newList = originalList.Where(item => item.ToString().ToLower().Contains(s));
+            }
+
+            // clear list (loop through it, otherwise the cursor would move to the beginning of the textbox...)
+            while (comboBox1.Items.Count > 0)
+            {
+                comboBox1.Items.RemoveAt(0);
+            }
+
+            //comboBox1.Items.Clear();
+            // re-set list
+            comboBox1.Items.AddRange(newList.ToArray());
+            comboBox1.DroppedDown = true;
+            //text= comboBox1.Text;
+
+            //comboBox1.Text = text;
+
+        }
+
+        private void comboBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            comboBox1.Text = comboBox1.SelectedItem.ToString();
+        }
+
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Text = comboBox1.SelectedItem.ToString();
+        }
+
+        private void comboBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            MessageBox.Show("Enter");
+        }
+
+        //private void comboBox1_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //  //  var index= (string)comboBox1.SelectedIndex.ToString();
+        //   // comboBox1.Text = 
+        //}
+    }
+}
